55d140f0e5109dfe8704380d27ff393e2a2fb9ff
Kael-Scott <144376622+Kael-Scott@users.noreply.github.com>
2024-03-28 23:45:28 +0000

    Added a temporary welcome message and menu

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-#import sqlite3
+import os
+import time
 
-print
+while True:
+    os.system('clear')
+    print(
+"""
+\\---------------------------------------------------/
+|                        Welcome!                   |
+|    Please select one of the following options:    |
+|                                                   |
+| 1 )  Create account                               |
+| 2 )  Log in                                       |
+|                                                   |
+/---------------------------------------------------\\
+"""
+    )
+    try:
+        choice = int(input("\nEnter a number:\n\n> "))
+        if choice == 1:
+            "Do something"
+        elif choice == 2:
+            "Do something else"
+        else:
+            raise ValueError
+        break
+    except ValueError:
+        print("Bad value! Try again.")
+        time.sleep(2)
